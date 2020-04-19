fcf778154f76c48c979463dc0f9cdf7e2cb3747d
Ansh Godha <ag759@cornell.edu>
2020-04-19 20:13:50 +0530

    initialized mli file, also changed up the makefile so it now builds card modules

--- card.mli
+++ card.mli
@@ -11,5 +11,19 @@ type money_card
 type rent_card
 type action_card
 type wildcard
+
+(* the type representing all cards *)
 type card
+
+val get_card_from_file: string -> (Yojson.Basic.t -> 'a) -> 'a list
+
+val get_money: unit -> money_card list
+
+val get_properties: unit -> property_card list
+
+val get_actions: unit -> action_card list
+
+val get_wildcards: unit -> wildcard list
+
+val get_rents: unit -> rent_card list
 
