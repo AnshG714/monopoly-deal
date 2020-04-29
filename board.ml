40cf8be3b649c1bd283b91680dc7552c1cf7af1d
Ansh Godha <ag759@cornell.edu>
2020-04-29 23:27:33 +0530

    added some methods to board

--- board.ml
+++ board.ml
@@ -5,16 +5,24 @@ open Player
 type board = 
   {
     players: player list;
-    deck: deck
+    deck: deck;
+    mutable turn: int
   }
 
 let rec init_mult_players n = 
   if (n = 0) then []
   else initialize_player() :: (init_mult_players (n-1))
 
-let board_init (n: int): board = 
+let initialize_board (n: int): board = 
   {
     players = init_mult_players n;
     deck = initialize_deck();
+    turn = 0;
   }
 
+(* This can be migrated to whatebver our controller will be. *)
+let increment_turn (board: board) = 
+  board.turn <- board.turn + 1
+
+let get_current_turn board = 
+  board.turn
