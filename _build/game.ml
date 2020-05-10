4687198a256c00598a69799b9ac337abf87f4f9d
Pooja <pg334@cornell.edu>
2020-05-09 22:23:10 -0400

    reformatting sly and forced deal

--- _build/game.ml
+++ _build/game.ml
@@ -65,7 +65,32 @@ let its_my_bday (board: board) =
   helper 2 others;
   true
 
-let rec sly_deal (board: board) = 
+let rec move_property board f name = 
+  let rec transfer_helper i = 
+    match 
+      transfer_card i 
+        (List.find (fun x -> get_player_name x = name) 
+           (get_players board))
+        (List.nth (get_players board) 
+           (get_current_turn board)) 
+    with
+    | exception InvalidCard -> print_endline "wrong"; loop ()
+    | _ -> true
+
+  and loop () = 
+    match read_line () with
+    | entry -> (match int_of_string_opt entry with
+        | Some i -> (if i < 25 || i > 52 then (print_endline "this isn't a property card!"; loop ())
+                     else transfer_helper i)
+        | None -> if entry = "back" then if f = sly_deal then sly_deal board else forced_deal board
+          else (print_endline "You need to either enter a valid id for the property card you want to take, or type 'back'."; 
+                loop ())) 
+    | exception Failure _ -> false in
+
+  loop ()
+
+
+and  sly_deal (board: board) = 
   print_endline "\027[38;5;190mYou have chosen to play a sly deal card. To do this, first enter enter the name of the person you want to perform the sly deal with. If you want to cancel, type 'cancel'. The players are: \027[0m";
   let name = get_player_name_input board in
   if name = "cancel" then false 
@@ -74,39 +99,9 @@ let rec sly_deal (board: board) =
      print_pile_of_player board name;
      print_endline "Either enter an id, or enter 'back' if you want to choose another player. ";
 
-     let rec transfer_helper i = 
-       match 
-         transfer_card i 
-           (List.find (fun x -> get_player_name x = name) 
-              (get_players board))
-           (List.nth (get_players board) 
-              (get_current_turn board)) 
-       with
-       | exception InvalidCard -> print_endline "wrong"; loop ()
-       | _ -> true
+     move_property board sly_deal name;)
 
-     and loop () = 
-       match read_line () with
-       | entry -> (match int_of_string_opt entry with
-           | Some i -> (if i < 25 || i > 52 then (print_endline "this isn't a property card!"; loop ())
-                        else transfer_helper i)
-           | None -> if entry = "back" then sly_deal board
-             else (print_endline "You need to either enter a valid id for the property card you want to take, or type 'back'."; 
-                   loop ())) 
-       | exception Failure _ -> false in
-
-     loop ())
-
-let debt_collector board = 
-  print_endline "\027[38;5;190mYou have chosen to play a debt collector card. To do this, enter enter the name of the person you want to collect debt from. The players are: \027[0m";
-  let name = get_player_name_input board in
-  if name = "cancel" then false else
-    (let player = List.find (fun x -> get_player_name x = name) (get_players board) in
-     print_pile_of_player board name;
-     ask_for_money board (List.nth (get_players board) (get_current_turn board)) player 5 0;
-     true)
-
-let rec forced_deal board =
+and  forced_deal board =
   let currpl = List.nth (get_players board) (get_current_turn board) in
   print_endline "\027[38;5;190mYou have chosen to play a forced deal card.";
   print_current_player_pile board;
@@ -116,8 +111,8 @@ let rec forced_deal board =
   else
     let rec swap_out p id = 
       (match int_of_string_opt id with
-       | Some i -> (try remove_card_from_personal_pile i p with _ -> print_endline "please enter a valid id"; let id = get_player_name_input board in swap_out p id)
-       | None -> print_endline "please enter a valid id"; let id = get_player_name_input board in swap_out p id;) in
+       | Some i -> (try remove_card_from_personal_pile i p with _ -> print_endline "please enter a valid id"; let id = read_line () in swap_out p id)
+       | None -> print_endline "please enter a valid id"; let id = read_line () in swap_out p id;) in
 
     let card_out = swap_out currpl id in 
 
@@ -128,28 +123,18 @@ let rec forced_deal board =
     print_pile_of_player board name;
     print_endline "Enter the id of the card you want to swap in";
 
-    let rec transfer_helper i = 
-      match 
-        transfer_card i 
-          (List.find (fun x -> get_player_name x = name) 
-             (get_players board))
-          (List.nth (get_players board) 
-             (get_current_turn board)) 
-      with
-      | exception InvalidCard -> print_endline "wrong"; loop ()
-      | _ -> true
+    if move_property board forced_deal name then add_card_to_personal_pile card_out player; true
 
-    and loop () = 
-      match read_line () with
-      | entry -> (match int_of_string_opt entry with
-          | Some i -> (if i < 25 || i > 52 then (print_endline "this isn't a property card!"; loop ())
-                       else transfer_helper i)
-          | None -> if entry = "back" then forced_deal board
-            else (print_endline "You need to either enter a valid id for the property card you want to take, or type 'back'."; 
-                  loop ())) 
-      | exception Failure _ -> false in
+let debt_collector board = 
+  print_endline "\027[38;5;190mYou have chosen to play a debt collector card. To do this, enter enter the name of the person you want to collect debt from. The players are: \027[0m";
+  let name = get_player_name_input board in
+  if name = "cancel" then false else
+    (let player = List.find (fun x -> get_player_name x = name) (get_players board) in
+     print_pile_of_player board name;
+     ask_for_money board (List.nth (get_players board) (get_current_turn board)) player 5 0;
+     true)
 
-    if loop () then add_card_to_personal_pile card_out player; true
+
 
 
 
